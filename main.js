dca4967b56d405d6c161eae1df2d424b87d1372c
Adam Wieckert <wiec1369@gmail.com>
2018-03-24 11:47:32 -0500

    Made stuff turn green. Wrapped somethings in a pretty startApplication function.

--- main.js
+++ main.js
@@ -56,8 +56,7 @@ cardBuilder(locations);
 
 const addDiaryEntry = (e) => {
     let textToCopy = e.target.previousSibling.value;
-    console.log("Value of event target: ", textToCopy);
-    stringToPrint = "";
+    let stringToPrint = "";
     stringToPrint += `<div class="entry">`;
     stringToPrint +=   `<h3>${e.target.parentElement.firstChild.innerHTML}</h3>`;
     stringToPrint +=   `<p class="entry-text">${textToCopy}</p>`;
